61fac2bf311378428c6fdd0a00a42e51ef7e9a78
MASTER <master@example.com>
2024-03-14 17:57:11 -0400

    minor formatting changes spacing/commenting changes and allowed modification of data in sendData

--- makefile
+++ makefile
@@ -1,14 +1,18 @@
 .PHONY: sender receiver
 
+all: sender receiver
+
 SENDER_ID   := .port.properties.serialNumber == "75932313638351300251" or .port.properties.pid == "0x003e"
 RECEIVER_ID := .port.properties.serialNumber == "85138313234351E09162"
 
 sender:
+	@echo "\033[1mcompiling and uploading $@\033[0m"
 	@FLAGS=$$(arduino-cli board list --format=json | jq -r '.[] | select($(SENDER_ID)) | "-b \(.matching_boards | .[0] | .fqbn) -p \(.port.address)"'); \
-	arduino-cli compile $$FLAGS $@; \
+	arduino-cli compile $$FLAGS $@ && \
 	arduino-cli upload $$FLAGS $@
 
 receiver:
+	@echo "\033[1mcompiling and uploading $@\033[0m"
 	@FLAGS=$$(arduino-cli board list --format=json | jq -r '.[] | select($(RECEIVER_ID)) | "-b \(.matching_boards | .[0] | .fqbn) -p \(.port.address)"'); \
-	arduino-cli compile $$FLAGS $@; \
+	arduino-cli compile $$FLAGS $@ && \
 	arduino-cli upload $$FLAGS $@
--- sender/sender.ino
+++ sender/sender.ino
@@ -1,9 +1,6 @@
-//Reads all traffic on CAN0 and forwards it to CAN1 (and in the reverse direction) but modifies some frames first.
-// Required libraries
 #include "variant.h"
 #include <due_can.h>
 
-//Leave defined if you use native port, comment if using programming port
 #define Serial SerialUSB
 #define HAMMER_POINT 89e-6
 #define SPEED CAN_BPS_250K
@@ -11,25 +8,21 @@
 volatile bool sof = false;
 
 void setup() {
-  pinMode(LED_BUILTIN, OUTPUT);
-  
   Serial.begin(115200);
+  pinMode(LED_BUILTIN, OUTPUT);
 
-  if (Can0.begin(SPEED)) {}
-  else {
-    Serial.println("CAN initialization (sync) ERROR");
-  }
+  Can0.begin(SPEED);
 
   PMC->PMC_PCER0 |= PMC_PCER0_PID11; // PIOA power ON
 
-  //Set CAN_RX as input (Ouput Disable Register) and
+  // set CAN_RX as input (Output Disable Register) and
   PIOA->PIO_PER = PIO_PA1A_CANRX0;
   PIOA->PIO_ODR = PIO_PA1A_CANRX0;
 
   PIOA->PIO_PDR = PIO_PA0A_CANTX0; // unable to send data w/ PER so changed to PDR (2024-03-13)
   PIOA->PIO_OER = PIO_PA0A_CANTX0;
 
-  //Disable pull-up on both pins (Pull Up Disable Register)
+  // disable pull-up on both pins (Pull Up Disable Register)
   PIOA->PIO_PUDR = PIO_PA1A_CANRX0;
   PIOA->PIO_PUDR = PIO_PA0A_CANTX0;
 
@@ -39,17 +32,10 @@ void setup() {
   delay(1000);
 
   Can0.watchFor();
-  attachInterrupt(PIO_PA1A_CANRX0, testFunc, FALLING);
-}
-
-void testFunc() {
-  digitalWrite(LED_BUILTIN, HIGH);
-  detachInterrupt(PIO_PA1A_CANRX0);
-  digitalWrite(LED_BUILTIN, LOW);
+  // attachInterrupt(PIO_PA1A_CANRX0, testFunc, FALLING);
 }
 
 void startTimer(Tc* tc, uint32_t channel, IRQn_Type irq, uint32_t frequency) { //  DO NOT TOUCH
-  
   //Enable or disable write protect of PMC registers.
   pmc_set_writeprotect(false);
   //Enable the specified peripheral clock.
@@ -65,11 +51,9 @@ void startTimer(Tc* tc, uint32_t channel, IRQn_Type irq, uint32_t frequency) { /
   tc->TC_CHANNEL[channel].TC_IER = TC_IER_CPCS;
   tc->TC_CHANNEL[channel].TC_IDR = ~TC_IER_CPCS;
   NVIC_EnableIRQ(irq);
-  
 }
-//
+
 //void CANdy_Sync() { // We will run TC3_Handler
-//  
 //  sof = true;
 //  NVIC_DisableIRQ(TC3_IRQn);
 //  startTimer(TC1, 0, TC3_IRQn, HAMMER_POINT); // This allows us to sample exactly at 20% (SAMPLING_POINT == 5) of the bit time (1/f = T). This is where the ringing will be done.
@@ -77,12 +61,11 @@ void startTimer(Tc* tc, uint32_t channel, IRQn_Type irq, uint32_t frequency) { /
 //}
 
 void TC3_Handler() {
-  
   TC_GetStatus(TC1, 0);
 
-//   PIO_PA1A_CANRX0 = If there is a bit
-//   PIO_PDSR = Time efficency (digial inputs)
-  bool value = PIOA->PIO_PDSR & PIO_PA0A_CANTX0;    // We read/sample the bit here
+  // PIO_PA1A_CANRX0 = If there is a bit
+  // PIO_PDSR = Time efficency (digial inputs)
+  bool value = PIOA->PIO_PDSR & PIO_PA0A_CANTX0; // We read/sample the bit here
 
   // sof = Start of Frame (goes through each frame) (Should be 0)
   // Stuff bit = If there is a row of 5 consistant bits, then add a stuff bit of the opposite bit
@@ -91,26 +74,22 @@ void TC3_Handler() {
   // No stuff bits after Acknlowedgement bit!
 
   PIOA->PIO_CODR = PIO_PA0A_CANTX0;
-
 }
 
-void sendData() { // THIS IS CORRECT DON'T TOUCH
+
+void sendData(const uint8_t* data, const int dataLength) {
   CAN_FRAME outgoing;
   outgoing.id = 0x400;
   outgoing.extended = false;
   outgoing.priority = 4;
-  outgoing.length = 8;
+  outgoing.length = dataLength;
 
-  outgoing.data.byte[0] = 0x00;
-  outgoing.data.byte[1] = 0x11;
-  outgoing.data.byte[2] = 0x22;
-  outgoing.data.byte[3] = 0x33;
-  outgoing.data.byte[4] = 0x44;
-  outgoing.data.byte[5] = 0x55;
-  outgoing.data.byte[6] = 0x66;
-  outgoing.data.byte[7] = 0x77;
+  for (int i = 0; i < dataLength; i++) {
+    outgoing.data.byte[i] = data[i];
+  }
 
   if (Can0.sendFrame(outgoing)) {
+    // if frame successfully sent, flip LED state
     digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
   }
 }
@@ -134,6 +113,9 @@ void sendData() { // THIS IS CORRECT DON'T TOUCH
 //}
 
 void loop() {
+  const int dataLength = 8;
+  const uint8_t data[dataLength] = {0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77};
+
   delay(1000);
-  sendData();
+  sendData(data, dataLength);
 }
