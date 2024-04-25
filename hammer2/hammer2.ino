05e42b305921e72030617bbb50a6a2edceb7bfec
MASTER <master@example.com>
2024-04-24 23:43:02 -0400

    implement basis for hammering actual writing of bits needs to be fixed to complete hammering

--- hammer2/hammer2.ino
+++ hammer2/hammer2.ino
@@ -4,15 +4,21 @@
 #define Serial SerialUSB
 
 #define SPEED        CAN_BPS_250K
-#define HAMMER_POINT 85e-6
+#define HAMMER_POINT 84.6e-6
+#define HAMMER_SIZE 2
 
 #define LED_HIGH() do { PIOB->PIO_SODR = PIO_PB27; ledState = true; } while(0)
 #define LED_LOW()  do { PIOB->PIO_CODR = PIO_PB27; ledState = false; } while(0)
 #define LED_FLIP() do { if (ledState) LED_LOW(); else LED_HIGH(); } while(0)
 
 volatile bool ledState = false;
+volatile bool sof = false;
+volatile bool resetValue = false;
 
 unsigned short int queue = 0b111111111111111;
+
+const bool hammerBits[HAMMER_SIZE] = {1, 0};
+volatile int hammerIndex = 0;
 
 void TC3_Handler() {
   TC_GetStatus(TC1, 0);
@@ -20,7 +26,60 @@ void TC3_Handler() {
   queue = queue << 1 | (PIOA->PIO_PDSR & PIO_PA1A_CANRX0) & 0b111111111111111;
 
   if (queue == 0b111111111111110) {
+    sof = true;
+    NVIC_DisableIRQ(TC6_IRQn);
+    startTimer(TC2, 0, TC6_IRQn, 1 / HAMMER_POINT);
+  }
+}
+
+void TC6_Handler() {
+  TC_GetStatus(TC2, 0);
+
+  if (!sof) {
+    stopTimer(TC6_IRQn);
+
+    return;
+  }
+
+  sof = false;
+
+  resetValue = PIOA->PIO_PDSR & PIO_PA1A_CANRX0;
+
+  // start hammering
+  hammerIndex = 0;
+
+  PIOA->PIO_PER = PIO_PA0A_CANTX0;
+  PIOA->PIO_OER = PIO_PA0A_CANTX0;
+
+  NVIC_DisableIRQ(TC7_IRQn);
+  startTimer(TC2, 1, TC7_IRQn, HAMMER_SIZE * SPEED);
+}
+
+void TC7_Handler() {
+  TC_GetStatus(TC2, 1);
+
+  if (hammerIndex < HAMMER_SIZE) {
+    CANdy_Write(hammerBits[hammerIndex]);
+    hammerIndex++;
+  } else {
+    CANdy_Write(resetValue);
+
+    // turn off multiplexing
+    PIOA->PIO_PDR = PIO_PA0A_CANTX0;
+    PIOA->PIO_ODR = PIO_PA0A_CANTX0;
+
     LED_FLIP();
+
+    stopTimer(TC7_IRQn);
+  }
+}
+
+void CANdy_Write(bool value) {
+  // TODO: fix this!!!
+  if (value) {
+    PIOA->PIO_SODR = PIO_PA0A_CANTX0;
+  } else {
+    PIOA->PIO_CODR = PIO_PA0A_CANTX0;
   }
 }
 
@@ -40,6 +99,11 @@ void startTimer(Tc* tc, uint32_t channel, IRQn_Type irq, uint32_t frequency) {
   tc->TC_CHANNEL[channel].TC_IER = TC_IER_CPCS;
   tc->TC_CHANNEL[channel].TC_IDR = ~TC_IER_CPCS;
   NVIC_EnableIRQ(irq);
+}
+
+void stopTimer(IRQn_Type irq) {
+	NVIC_DisableIRQ(irq);
+	pmc_disable_periph_clk((uint32_t)irq);
 }
 
 void setup() {
