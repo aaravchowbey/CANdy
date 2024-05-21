17b60cbb38a09d150667cd40f1f9146ad5b358db
MASTER <master@example.com>
2024-05-21 09:46:57 -0400

    fix spacing and comments + reset frameBitHammered value unnecessary spaces removed comments capitalization and formatting fixed frameBitHammered value is now reset in TC6_Handler LED macros condensed to one macro now

--- hammer2/hammer2.ino
+++ hammer2/hammer2.ino
@@ -1,20 +1,21 @@
-#include "variant.h"
 #include <due_can.h>
 
 #define Serial SerialUSB
 
 #define SPEED              CAN_BPS_50K
-#define HAMMER_BIT_COUNT   2    //total number of bits to hammer for frame
-#define HAMMER_SIZE        2    //number of bits to hammer per bit 
-#define SAMPLING_RATE      10   
+#define HAMMER_BIT_COUNT   2           // total number of bits to hammer for message frame
+#define HAMMER_SIZE        2           // number of bits to hammer per data bit
+#define SAMPLING_RATE      10          // sampling rate of CAN bus
 
-#define LED_HIGH() do { PIOB->PIO_SODR = PIO_PB27; ledState = true; } while(0)
-#define LED_LOW()  do { PIOB->PIO_CODR = PIO_PB27; ledState = false; } while(0)
-#define LED_FLIP() do { if (ledState) LED_LOW(); else LED_HIGH(); } while(0)
+#define SET_LED(state)                                                         \
+  do {                                                                         \
+    (state) ? (PIOB->PIO_SODR = PIO_PB27) : (PIOB->PIO_CODR = PIO_PB27);       \
+    ledState = state;                                                          \
+  } while (0)
 
 #define ONE_BITS(x) ((1ULL << (x)) - 1)
-#define BITS_AT_POS(queue, pos, num_bits)                              \
-  (queue[pos * SAMPLING_RATE / 64] &                                   \
+#define BITS_AT_POS(queue, pos, num_bits)                                      \
+  (queue[pos * SAMPLING_RATE / 64] &                                           \
    (ONE_BITS(num_bits * SAMPLING_RATE) << (SAMPLING_RATE * pos % 64)))
 
 volatile bool ledState = false;
@@ -43,7 +44,7 @@ void TC3_Handler() {
   bus_queue[1] = ((bus_queue[1] & ONE_BITS(63)) << 1) | ((bus_queue[0] & (1ULL << 63)) >> 63);
   bus_queue[0] = ((bus_queue[0] & ONE_BITS(63)) << 1) | value;
 
-  if ((bus_queue[2] & ONE_BITS(22)) == ONE_BITS(22) && bus_queue[1] == UINT64_MAX && bus_queue[0] == (ONE_BITS(54) << 10)) { 
+  if ((bus_queue[2] & ONE_BITS(22)) == ONE_BITS(22) && bus_queue[1] == UINT64_MAX && bus_queue[0] == (ONE_BITS(54) << 10)) {
     // check if 140 ones followed by 10 zeros is found in last 150 bits
     // ([2] has 22 ones, [1] has 64 ones, [0] has 54 ones & 10 zeros)
     sof = true;
@@ -67,11 +68,11 @@ void TC3_Handler() {
         (frame_queue[0] & (ONE_BITS(4) << 60)) == 0 && //                                (last 4 samples)
         BITS_AT_POS(frame_queue, 5, 1) == 0 &&         // IDE
         BITS_AT_POS(frame_queue, 4, 1) == 0 &&         // reserved bit
-        BITS_AT_POS(frame_queue, 0, 4) != 0            // DLC (must be >0)
+        BITS_AT_POS(frame_queue, 0, 4) != 0            // DLC (must be > 0)
     ) {
       frame_queue[2] = frame_queue[1] = frame_queue[0] = UINT64_MAX;
       sof = false;
-      LED_FLIP();
+      SET_LED(!ledState);
 
       CANdy_Hammer();
     }
@@ -81,7 +82,8 @@ void TC3_Handler() {
 // starts hammering
 void CANdy_Hammer() {
   hammerIndex = 0;
-  
+
+  // FIX: starts hammering in second data bit
   startTimer(TC2, 0, TC6_IRQn, SPEED);
 }
 
@@ -92,6 +94,7 @@ void TC6_Handler() {
   if (hammerIndex < HAMMER_BIT_COUNT) {
     // if bits left to hammer, setup hammering for one data bit
     resetValue = (PIOA->PIO_PDSR & PIO_PA1A_CANRX0) != 0;
+    frameBitHammered = false;
 
     // turn on multiplexing
     PIOA->PIO_PER = PIO_PA0A_CANTX0;
@@ -107,14 +110,14 @@ void TC6_Handler() {
 
 // handles hammering bits for one data bit
 void TC7_Handler() {
-  TC_GetStatus(TC2, 1); 
+  TC_GetStatus(TC2, 1);
 
   if (!frameBitHammered) {
     // if bit in frame has not been completely hammered, continue with hammering
     CANdy_Write(hammerData[hammerIndex]);
 
     hammerIndex++;
-    
+
     if (hammerIndex % HAMMER_SIZE == 0 || hammerIndex == HAMMER_BIT_COUNT) {
       frameBitHammered = true;
     }
@@ -130,32 +133,6 @@ void TC7_Handler() {
     PIOA->PIO_ODR = PIO_PA0A_CANTX0;
   }
 }
-
-// void TC6_Handler() {
-//   TC_GetStatus(TC2, 0);
-//
-//   if (!sof) {
-//     stopTimer(TC2, 0, TC6_IRQn);
-//
-//     return;
-//   }
-//
-//   sof = false;
-//
-//   resetValue = (PIOA->PIO_PDSR & PIO_PA1A_CANRX0) != 0;
-//
-//   // start hammering
-//   hammerIndex = 0;
-//
-//   LED_FLIP();
-//   PIOA->PIO_PER = PIO_PA0A_CANTX0;
-//   PIOA->PIO_OER = PIO_PA0A_CANTX0;
-//
-//   startTimer(TC2, 1, TC7_IRQn, 4000000);
-//
-//   PIOA->PIO_PDR = PIO_PA0A_CANTX0;
-//   PIOA->PIO_ODR = PIO_PA0A_CANTX0;
-// }
 
 void CANdy_Write(bool value) {
   if (value) {
@@ -186,37 +163,39 @@ void startTimer(Tc* tc, uint32_t channel, IRQn_Type irq, uint32_t frequency) {
 void stopTimer(Tc* tc, uint32_t channel, IRQn_Type irq) {
   NVIC_DisableIRQ(irq);
   TC_Stop(tc, channel);
-} 
-
+}
 
 void setup() {
-  // start serial port at 115200 bps:
+  // start serial port at 115200 bps
   Serial.begin(115200);
 
-  // Verify CAN0 and CAN1 initialization, baudrate is 1Mb/s:
+  // CAN0 and CAN1 initialization, baudrate is 1Mb/s
   Can0.begin(SPEED);
   Can1.begin(SPEED);
- 
-  PMC->PMC_PCER0 |= PMC_PCER0_PID11; // PIOA power ON
 
-  //Multiplex CAN_RX to GPIO (Peripheral Enable Register)
+  // PIOA power ON
+  PMC->PMC_PCER0 |= PMC_PCER0_PID11;
+
+  // multiplex CAN_RX to GPIO (Peripheral Enable Register)
   PIOA->PIO_PER = PIO_PA1A_CANRX0;
 
-  //Set CAN_RX as input (Ouput Disable Register)
+  // set CAN_RX as input (Output Disable Register)
   PIOA->PIO_ODR = PIO_PA1A_CANRX0;
 
-  //Disable pull-up on both pins (Pull Up Disable Register)
+  // disable pull-up on both pins (Pull Up Disable Register)
   PIOA->PIO_PUDR = PIO_PA1A_CANRX0;
   PIOA->PIO_PUDR = PIO_PA0A_CANTX0;
 
+  // PIOB power ON
+  PMC->PMC_PCER0 |= PMC_PCER0_PID12;
+
   // enable control of LEDs
-  PMC->PMC_PCER0 |= PMC_PCER0_PID12; // PIOB power ON
   PIOB->PIO_PER = PIO_PB27;
   PIOB->PIO_OER = PIO_PB27;
 
-  LED_HIGH();
+  SET_LED(true);
   delay(500);
-  LED_LOW();
+  SET_LED(false);
   delay(1000);
 
   Can0.watchFor();
