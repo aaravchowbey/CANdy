d576a1de8b6dfb6078da41dceee4d2b9f7ae71b4
MASTER <master@example.com>
2024-05-24 15:48:14 -0400

    use timestamp instead of sampling for SOF modify due_can library for CANdy and use timestamp to start timer for counting bits until hammering

--- hammer2/hammer2.ino
+++ hammer2/hammer2.ino
@@ -1,4 +1,5 @@
-#include <due_can.h>
+
+#include <due_can-CANdy.h>
 
 #define Serial SerialUSB
 
@@ -17,28 +18,27 @@
   do {                                                                         \
     (value) ? (PIOB->PIO_SODR = PIO_PB26) : (PIOB->PIO_CODR = PIO_PB26);       \
   } while (0)
-
 // PIO_PA0A_CANTX0
 
 volatile bool resetValue = false;
 
-uint64_t bus_queue = UINT64_MAX;
-uint32_t frame_queue = UINT32_MAX;
+volatile uint64_t bus_queue = UINT64_MAX;
+volatile uint32_t frame_queue = 0b11111111111111111111111111111110;
 
 // data bits to hammer
-const bool hammerData[HAMMER_BIT_COUNT] = {0, 1};
+const uint64_t hammerData = 0b01;
 
 // current index of hammerData
-volatile int hammerIndex = 0;
+volatile uint8_t hammerIndex = 0;
 
 // bit in frame has been completely hammered
 volatile bool frameBitHammered = false;
 
 volatile bool sof = false;
-volatile uint8_t frame_samples = 0;
+volatile uint8_t frame_samples = 1;
 
-void TC3_Handler() {
-  TC_GetStatus(TC1, 0);
+void TC0_Handler() {
+  TC_GetStatus(TC0, 0);
 
   bool value = PIOA->PIO_PDSR & PIO_PA1A_CANRX0;
 
@@ -53,117 +53,9 @@ void TC3_Handler() {
     frame_samples = 2;
     hammerIndex = 0;
 
-    stopTimer(TC1, 0, TC3_IRQn);
     startTimer(TC1, 2, TC5_IRQn, SPEED);
+    stopTimer(TC0, 0, TC0_IRQn);
   }
-
-  //  else {
-  //   // else in frame
-  //   // check if frame_queue has a stuff bit
-  //   uint64_t result = frame_queue[1] & 0x1FFFFFF;
-  //   if (frame_samples >= 25 && ((!value && result == 0x1FFFFFF) || (value && result == 0))) {
-  //     stuff_bit_counter--;
-  //
-  //     if (stuff_bit_counter == 0) {
-  //       stuff_bit_counter = 5;
-  //       frame_samples = 0;
-  //     }
-  //   } else {
-  //     // if not stuff bit, add to frame_queue
-  //     frame_queue[0] = ((frame_queue[0] & 0x7FFFFFFFFFFFFFFF) << 1) |
-  //                      ((frame_queue[1] & 0x8000000000000000) >> 63);
-  //     frame_queue[1] = ((frame_queue[1] & 0x7FFFFFFFFFFFFFFF) << 1) |
-  //                      value;
-  //
-  //     frame_samples++;
-  //
-  //     if (frame_samples >= 95) {
-  //       sof = false;
-  //     } else if ((frame_queue[1] & 0xFFFFF) != 0 && // DLC (must be > 0)
-  //                (frame_queue[1] & 0x7FFF00000) == 0 && // reserved bit (must be 0)
-  //                (frame_queue[0] & 0x7C000000) == 0 // start of frame
-  //     ) {
-  //       sof = false;
-  //       CANdy_Hammer();
-  //     }
-  //   }
-  // }
-
-  // if (!sof) {
-  //   // SET_LED(false);
-  //   // if not sof, add to bus_queue
-  //   bus_queue[0] = ((bus_queue[0] & 0x7FFFFFFFFFFFFFFF) << 1) | ((bus_queue[1] & 0x8000000000000000) >> 63);
-  //   bus_queue[1] = ((bus_queue[1] & 0x7FFFFFFFFFFFFFFF) << 1) | value;
-  //
-  //   // check if bus_queue contains sof
-  //   if ((bus_queue[0] & 0x3FFFFFFFFFFF) == 0x3FFFFFFFFFFF &&
-  //       bus_queue[1] == 0xFFFFFFFFFFFFFC00) {
-  //     sof = true;
-  //     frame_queue[0] = UINT64_MAX;
-  //     frame_queue[1] = UINT64_MAX;
-  //     frame_queue[2] = 0xFFFFFFFFFFFFFC00;
-  //     frame_samples = 10;
-  //     stuff_bit_counter = 10;
-  //   }
-  // }
-  // } return; if(true) /*else*/ {
-  //   // else in frame
-  //
-  //   // FIX: below shows CANdy_Hammer does not work properly
-  //   // sof = false;
-  //   // CANdy_Hammer();
-  //   // return;
-  //   // Serial.println((frame_queue[2] & 0x3FF) == 0x3FF);
-  //
-  //   // FIX: below shows that value is broken!
-  //   // if (frame_samples == 10) {
-  //   //   SET_LED(true);
-  //   // } else if ((frame_queue[2] & 0x3FF) == 0x3FF) {
-  //   //   SET_LED(false);
-  //   //   sof = false;
-  //   // }
-  //   // return;
-  //   // Serial.println(value);
-  //   // Serial.print((uint32_t)(frame_queue[2] & UINT32_MAX));
-  //   // Serial.print(" ");
-  //   // Serial.print(frame_samples);
-  //   // Serial.print("\n");
-  //
-  //   // check if frame_queue has a stuff bit
-  //   uint64_t result = (frame_queue[2] & 0x3FFFFFFFFFFFF);
-  //   if (frame_samples >= 50 && ((!value && result == 0x3FFFFFFFFFFFF) || (value && result == 0))) {
-  //     // SET_LED(false);
-  //     stuff_bit_counter--;
-  //
-  //     if (stuff_bit_counter == 0) {
-  //       stuff_bit_counter = 10;
-  //       frame_samples = 0;
-  //     }
-  //   } else {
-  //     // if not stuff bit, add to frame_queue
-  //     frame_queue[0] = ((frame_queue[0] & 0x7FFFFFFFFFFFFFFF) << 1) |
-  //                      ((frame_queue[1] & 0x8000000000000000) >> 63);
-  //     frame_queue[1] = ((frame_queue[1] & 0x7FFFFFFFFFFFFFFF) << 1) |
-  //                      ((frame_queue[2] & 0x8000000000000000) >> 63);
-  //     frame_queue[2] = ((frame_queue[2] & 0x7FFFFFFFFFFFFFFF) << 1) | value;
-  //
-  //     frame_samples++;
-  //
-  //     if (frame_samples >= 190) {
-  //       sof = false;
-  //       // SET_LED(false);
-  //       // CANdy_Hammer();
-  //     } else if ((frame_queue[2] & 0xFFFFFFFFFF) != 0 && // DLC (must be > 0)
-  //                (frame_queue[2] & 0xFFFFFF0000000000) == 0 && // reserved bit (must be 0)
-  //                (frame_queue[1] & 0x3F) == 0 && // RTR (0 for data frames) (first 6 samples)
-  //                (frame_queue[0] & 0x3FF0000000000000) == 0 // start of frame
-  //     ) {
-  //       sof = false;
-  //       // SET_LED(false);
-  //       // CANdy_Hammer();
-  //     }
-  //   }
-  // }
 }
 
 void TC5_Handler() {
@@ -171,14 +63,14 @@ void TC5_Handler() {
 
   bool value = PIOA->PIO_PDSR & PIO_PA1A_CANRX0;
 
-  if (frame_samples >= 5 && ((value == 1 && (frame_queue & 0b11111) == 0b11111) || (value == 0 && (frame_queue & 0b11111) == 0))) {
-    // SET_LED(true);
+  if (frame_samples >= 5 && ((value == 0 && (frame_queue & 0b11111) == 0b11111) || (value == 1 && (frame_queue & 0b11111) == 0))) {
+    // if stuff bit, reset frame_samples
     frame_samples = 0;
   } else {
-    // SET_LED(false);
     // if not stuff bit, add to frame_queue
     frame_queue = ((frame_queue & 0x7FFFFFFF) << 1) | value;
-    // SET_LED(frame_samples % 2 == 0);
+
+    SET_LED(frame_samples % 2 == 0);
 
     frame_samples++;
 
@@ -187,78 +79,21 @@ void TC5_Handler() {
         (frame_queue & 0b1110000) == 0 && // reserved bit + IDE + RTR (must be 0)
         (frame_queue & 0x40000) == 0 // start of frame
     ) {
-      // stopTimer(TC1, 2, TC5_IRQn);
-      startTimer(TC2, 0, TC6_IRQn, SPEED);
-    } else if ((frame_queue & 0b11111111) == 0b11111111) {
-      startTimer(TC1, 0, TC3_IRQn, SPEED * 5);
+      // if DLC complete, start hammering timer for incoming data bit
+      // startTimer(TC2, 0, TC6_IRQn, SPEED);
+    } else if (frame_samples >= 8 && (frame_queue & 0b11111111) == 0b11111111) {
+      // if end of frame, stop frame timer and starting sof detection timer
+      frame_queue = 0b11111111111111111111111111111110;
+      frame_samples = 1;
+      // startTimer(TC0, 0, TC0_IRQn, SPEED * 5);
       stopTimer(TC1, 2, TC5_IRQn);
     }
   }
 }
 
-// void TC3_Handler() {
-//   TC_GetStatus(TC1, 0);
-//
-//   bool value = (PIOA->PIO_PDSR & PIO_PA1A_CANRX0) != 0;
-//
-//   bus_queue[2] = ((bus_queue[2] & ONE_BITS(63)) << 1) | ((bus_queue[1] & ((uint64_t)1 << 63)) >> 63);
-//   bus_queue[1] = ((bus_queue[1] & ONE_BITS(63)) << 1) | ((bus_queue[0] & ((uint64_t)1 << 63)) >> 63);
-//   bus_queue[0] = ((bus_queue[0] & ONE_BITS(63)) << 1) | value;
-//
-//   if (!sof && (bus_queue[2] & ONE_BITS(22)) == ONE_BITS(22) && bus_queue[1] == UINT64_MAX && bus_queue[0] == (ONE_BITS(54) << 10)) {
-//     // check if 140 ones followed by 10 zeros is found in last 150 bits
-//     // ([2] has 22 ones, [1] has 64 ones, [0] has 54 ones & 10 zeros)
-//     sof = true;
-//     frame_queue[0] = ONE_BITS(54) << 10;
-//   } else if (sof) {
-//     if ((frame_queue[2] & ((uint64_t)1 << 63)) == 0) {
-//       sof = false;
-//       return;
-//     }
-//
-//     // if sof is true, continue until data bits in frame reached
-//     if (!(value == 0 && (frame_queue[0] & ONE_BITS(50)) == ONE_BITS(50) &&  // value is 1 and previous 50 samples are 0s (and is not in default state)
-//           frame_queue[0] != UINT64_MAX) &&
-//         !(value == 1 && (frame_queue[0] & ONE_BITS(50)) == 0)               // value is 0 and previous 50 samples are 1s
-//     ) {
-//       // if not stuff bit, add to frame_queue
-//       frame_queue[2] = ((frame_queue[2] & ONE_BITS(63)) << 1) | ((frame_queue[1] & ((uint64_t)1 << 63)) >> 63);
-//       frame_queue[1] = ((frame_queue[1] & ONE_BITS(63)) << 1) | ((frame_queue[0] & ((uint64_t)1 << 63)) >> 63);
-//
-//       // FIX: requires explicit conversion to uint64_t for some reason; why?
-//       frame_queue[0] = ((frame_queue[0] & ONE_BITS(63)) << 1) | ((uint64_t)(value ? 1 : 0));
-//     }
-//
-//     if (
-//         BITS_AT_POS(frame_queue, 18, 1) == 0 &&        // start-of-frame
-//         (frame_queue[1] & ONE_BITS(6)) == 0 &&         // RTR (must be 0 for data frame) (first 6 samples)
-//         (frame_queue[0] & (ONE_BITS(4) << 60)) == 0 && //                                (last 4 samples)
-//         BITS_AT_POS(frame_queue, 5, 1) == 0 &&         // IDE
-//         BITS_AT_POS(frame_queue, 4, 1) == 0 &&         // reserved bit
-//         BITS_AT_POS(frame_queue, 0, 4) != 0            // DLC (must be > 0)
-//         // (frame_queue[2] & (ONE_BITS(10) << 52)) == 0 &&
-//         // (frame_queue[1] & ONE_BITS(6)) == 0 &&         // RTR (must be 0 for data frame) (first 6 samples)
-//         // (frame_queue[0] & (ONE_BITS(4) << 60)) == 0 && //                                (last 4 samples)
-//         // (frame_queue[0] & (ONE_BITS(10) << 50)) == 0 &&
-//         // (frame_queue[0] & (ONE_BITS(10) << 40)) == 0 &&
-//         // (frame_queue[0] & ONE_BITS(40)) != 0
-//     ) {
-//       frame_queue[2] = frame_queue[1] = frame_queue[0] = UINT64_MAX;
-//       sof = false;
-//
-//       SET_LED(!ledState);
-//
-//       CANdy_Hammer();
-//     }
-//   }
-//   SET_LED(sof);
-// }
-
 // handles hammering bits for all data bits
 void TC6_Handler() {
   TC_GetStatus(TC2, 0);
-
-  // SET_LED(true);
 
   if (hammerIndex < HAMMER_BIT_COUNT) {
     // Serial.println(hammerIndex);
@@ -284,7 +119,7 @@ void TC7_Handler() {
 
   if (!frameBitHammered) {
     // if bit in frame has not been completely hammered, continue with hammering
-    CANdy_Write(hammerData[hammerIndex]);
+    CANdy_Write((hammerData & (1 << (HAMMER_SIZE - hammerIndex - 1))) >> hammerIndex);
 
     hammerIndex++;
 
@@ -302,24 +137,62 @@ void TC7_Handler() {
   }
 }
 
-void startTimer(Tc* tc, uint32_t channel, IRQn_Type irq, uint32_t frequency) {
-  //Enable or disable write protect of PMC registers.
-  pmc_set_writeprotect(false);
-  //Enable the specified peripheral clock.
-  pmc_enable_periph_clk((uint32_t)irq);
+// void CAN0_Handler() {
+//   Can0.interruptHandler();
+//   uint32_t status = CAN0->CAN_SR;
+//   // if (status & CAN_SR_RXRDY) {
+//   //   // Frame received successfully, possible EOF
+//   //   uint16_t timestamp = CAN0->CAN_TIM;
+//   //   // Process EOF
+//   //   Serial.print("EOF detected at: ");
+//   //   Serial.println(timestamp);
+//   // }
+//   // if (status & CAN_SR_TXOK) {
+//   //   // Frame transmitted successfully, possible SOF
+//   //   uint16_t timestamp = CAN0->CAN_TIM;
+//   //   // Process SOF
+//   //   Serial.print("SOF detected at: ");
+//   //   Serial.println(timestamp);
+//   // }
+// }
+//
+// void CAN1_Handler() {
+//   Can1.interruptHandler();
+//   uint32_t status = CAN1->CAN_SR;
+//   // if (status & CAN_SR_RXOK) {
+//   //   // Frame received successfully, possible EOF
+//   //   uint16_t timestamp = CAN1->CAN_TIM;
+//   //   // Process EOF
+//   //   Serial.print("EOF detected at: ");
+//   //   Serial.println(timestamp);
+//   // }
+//   // if (status & CAN_SR_TXOK) {
+//   //   // Frame transmitted successfully, possible SOF
+//   //   uint16_t timestamp = CAN1->CAN_TIM;
+//   //   // Process SOF
+//   //   Serial.print("SOF detected at: ");
+//   //   Serial.println(timestamp);
+//   // }
+// }
 
-  TC_Configure(tc, channel, TC_CMR_WAVE | TC_CMR_WAVSEL_UP_RC | TC_CMR_TCCLKS_TIMER_CLOCK4);
-  uint32_t rc = VARIANT_MCK / 128 / frequency;
-
-  TC_SetRA(tc, channel, rc / 2);
-  TC_SetRC(tc, channel, rc);
-  TC_Start(tc, channel);
-
-  tc->TC_CHANNEL[channel].TC_IER = TC_IER_CPCS;
-  tc->TC_CHANNEL[channel].TC_IDR = ~TC_IER_CPCS;
-  NVIC_EnableIRQ(irq);
-}
-
+// void startTimer(Tc* tc, uint32_t channel, IRQn_Type irq, uint32_t frequency) {
+//   //Enable or disable write protect of PMC registers.
+//   pmc_set_writeprotect(false);
+//   //Enable the specified peripheral clock.
+//   pmc_enable_periph_clk((uint32_t)irq);
+//
+//   TC_Configure(tc, channel, TC_CMR_WAVE | TC_CMR_WAVSEL_UP_RC | TC_CMR_TCCLKS_TIMER_CLOCK4);
+//   uint32_t rc = VARIANT_MCK / 128 / frequency;
+//
+//   TC_SetRA(tc, channel, rc / 2);
+//   TC_SetRC(tc, channel, rc);
+//   TC_Start(tc, channel);
+//
+//   tc->TC_CHANNEL[channel].TC_IER = TC_IER_CPCS;
+//   tc->TC_CHANNEL[channel].TC_IDR = ~TC_IER_CPCS;
+//   NVIC_EnableIRQ(irq);
+// }
+//
 void stopTimer(Tc* tc, uint32_t channel, IRQn_Type irq) {
   NVIC_DisableIRQ(irq);
   TC_Stop(tc, channel);
@@ -332,6 +205,12 @@ void setup() {
   // CAN0 and CAN1 initialization
   Can0.begin(SPEED);
   Can1.begin(SPEED);
+
+  Can0.enable_time_triggered_mode();
+  Can0.set_timestamp_capture_point(0);
+
+  Can1.enable_time_triggered_mode();
+  Can1.set_timestamp_capture_point(1);
 
   // PIOA power ON
   PMC->PMC_PCER0 |= PMC_PCER0_PID11;
@@ -364,11 +243,8 @@ void setup() {
 
   Can0.watchFor();
 
-  startTimer(TC1, 0, TC3_IRQn, SPEED * 5);
+  // startTimer(TC0, 0, TC0_IRQn, SPEED * 5);
 }
 
 void loop() {
-  // int i = micros();
-  // TC3_Handler();
-  // Serial.println(micros() - i);
 }
