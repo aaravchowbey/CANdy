42b71377998b040116e15e6ed865c1786d76c133
MASTER <master@example.com>
2024-05-28 16:47:02 -0400

    migrate away from due_can-CANdy also add config.mk for customization

--- hammer2/hammer2.ino
+++ hammer2/hammer2.ino
@@ -1,28 +1,29 @@
-
-#include <due_can-CANdy.h>
-
 #define Serial SerialUSB
 
-#define SPEED              CAN_BPS_50K
-#define HAMMER_BIT_COUNT   2           // total number of bits to hammer for message frame
-#define HAMMER_SIZE        2           // number of bits to hammer per data bit
-#define SAMPLING_RATE      5           // sampling rate of CAN bus
+#define SPEED CAN_BPS_50K
 
-#define SET_LED(state)                                                         \
-  do {                                                                         \
-    (state) ? (PIOB->PIO_SODR = PIO_PB27) : (PIOB->PIO_CODR = PIO_PB27);       \
-    (state) ? (PIOB->PIO_SODR = PIO_PB26) : (PIOB->PIO_CODR = PIO_PB26);       \
+#define HAMMER_BIT_COUNT 2  // total number of bits to hammer for message frame
+#define HAMMER_SIZE 2       // number of bits to hammer per data bit
+#define SAMPLING_RATE 5     // sampling rate of CAN bus
+
+#define SET_LED(state) \
+  do { \
+    (state) ? (PIOB->PIO_SODR = PIO_PB27) : (PIOB->PIO_CODR = PIO_PB27); \
+    (state) ? (PIOB->PIO_SODR = PIO_PB26) : (PIOB->PIO_CODR = PIO_PB26); \
   } while (0)
 
 #define CANdy_Write(value) \
-  do {                                                                         \
-    (value) ? (PIOB->PIO_SODR = PIO_PB26) : (PIOB->PIO_CODR = PIO_PB26);       \
+  do { \
+    (value) ? (PIOB->PIO_SODR = PIO_PB26) : (PIOB->PIO_CODR = PIO_PB26); \
   } while (0)
 // PIO_PA0A_CANTX0
+
+volatile bool hammer_state = false;
 
 volatile bool resetValue = false;
 
 volatile uint32_t frame_queue = 0b11111111111111111111111111111110;
+volatile int frame_samples = 2;
 
 // data bits to hammer
 const uint32_t hammerData = 0b01;
@@ -33,46 +34,53 @@ volatile uint8_t hammerIndex = 0;
 // bit in frame has been completely hammered
 volatile bool frameBitHammered = false;
 
-volatile bool sof = false;
-volatile uint8_t frame_samples = 1;
+void CAN0_Handler() {
+  if (CAN0->CAN_SR & CAN_SR_TSTP) {
+    // fires at second frame bit at ~70%
+    bool value = PIOA->PIO_PDSR & PIO_PA1A_CANRX0;
+    frame_queue = 0b11111111111111111111111111111100 | (value ? 1 : 0);
+    frame_samples = 2;
+
+    startTimer(TC0, 0, TC0_IRQn, SPEED * 1000);
+  }
+}
 
 // samples bits to determine when data bits start
-void TC5_Handler() {
-  TC_GetStatus(TC1, 2);
+void TC0_Handler() {
+  TC_GetStatus(TC1, 0);
 
-  bool value = PIOA->PIO_PDSR & PIO_PA1A_CANRX0;
-
+  bool value = (PIOA->PIO_PDSR & PIO_PA1A_CANRX0);
   if (frame_samples >= 5 && ((value == 0 && (frame_queue & 0b11111) == 0b11111) || (value == 1 && (frame_queue & 0b11111) == 0))) {
     // if stuff bit, reset frame_samples
     frame_samples = 0;
   } else {
     // if not stuff bit, add to frame_queue
     frame_queue = ((frame_queue & 0x7FFFFFFF) << 1) | value;
-
-    SET_LED(frame_samples % 2 == 0);
-
     frame_samples++;
 
-    if (hammerIndex == 0 &&
-        (frame_queue & 0b1111) != 0 && // DLC (must be > 0)
-        (frame_queue & 0b1110000) == 0 && // reserved bit + IDE + RTR (must be 0)
-        (frame_queue & 0x40000) == 0 // start of frame
+    // SET_LED(frame_samples % 2 == 1);
+
+    if (hammerIndex == 0 && (frame_queue & 0b1111) != 0 &&  // DLC (must be > 0)
+        (frame_queue & 0b1110000) == 0 &&                   // reserved bit + IDE + RTR (must be 0)
+        (frame_queue & 0x40000) == 0                        // start of frame
     ) {
       // if DLC complete, start hammering timer for incoming data bit
-      startTimer(TC2, 0, TC6_IRQn, SPEED);
+      // startTimer(TC1, 0, TC3_IRQn, SPEED * 1000);
+      CANdy_Write(hammer_state);
+      hammer_state = !hammer_state;
     } else if (frame_samples >= 8 && (frame_queue & 0b11111111) == 0b11111111) {
-      // if end of frame, stop frame timer and starting sof detection timer
-      frame_queue = 0b11111111111111111111111111111110;
-      frame_samples = 1;
-      // startTimer(TC0, 0, TC0_IRQn, SPEED * 5);
-      stopTimer(TC1, 2, TC5_IRQn);
+      // if end of frame, stop frame timer
+
+      frame_queue = UINT32_MAX;
+      frame_samples = 0;
+      stopTimer(TC0, 0, TC0_IRQn);
     }
   }
 }
 
 // handles hammering bits for all data bits
-void TC6_Handler() {
-  TC_GetStatus(TC2, 0);
+void TC3_Handler() {
+  TC_GetStatus(TC1, 0);
 
   if (hammerIndex < HAMMER_BIT_COUNT) {
     // if bits left to hammer, setup hammering for one data bit
@@ -84,16 +92,16 @@ void TC6_Handler() {
     // PIOA->PIO_OER = PIO_PA0A_CANTX0;
 
     // start TC7 to fire 5 times in a bit
-    startTimer(TC2, 1, TC7_IRQn, SPEED * 5);
+    startTimer(TC2, 0, TC6_IRQn, SPEED * 1000 * 5);
   } else {
     // stop timer when no more bits to hammer
-    stopTimer(TC2, 0, TC6_IRQn);
+    stopTimer(TC1, 0, TC3_IRQn);
   }
 }
 
 // handles hammering bits for one data bit
-void TC7_Handler() {
-  TC_GetStatus(TC2, 1);
+void TC6_Handler() {
+  TC_GetStatus(TC2, 0);
 
   if (!frameBitHammered) {
     // if bit in frame has not been completely hammered, continue with hammering
@@ -103,8 +111,8 @@ void TC7_Handler() {
 
     frameBitHammered = hammerIndex % HAMMER_SIZE == 0 || hammerIndex == HAMMER_BIT_COUNT;
   } else {
-    // stop TC7
-    stopTimer(TC2, 1, TC7_IRQn);
+    // stop TC6
+    stopTimer(TC2, 0, TC6_IRQn);
 
     // reset value
     CANdy_Write(resetValue);
@@ -115,7 +123,25 @@ void TC7_Handler() {
   }
 }
 
-void stopTimer(Tc* tc, uint32_t channel, IRQn_Type irq) {
+void startTimer(Tc *tc, uint32_t channel, IRQn_Type irq, uint32_t frequency) {
+  //Enable or disable write protect of PMC registers.
+  pmc_set_writeprotect(false);
+  //Enable the specified peripheral clock.
+  pmc_enable_periph_clk((uint32_t)irq);
+
+  TC_Configure(tc, channel, TC_CMR_WAVE | TC_CMR_WAVSEL_UP_RC | TC_CMR_TCCLKS_TIMER_CLOCK4);
+  uint32_t rc = VARIANT_MCK / 128 / frequency;
+
+  TC_SetRA(tc, channel, rc / 2);
+  TC_SetRC(tc, channel, rc);
+  TC_Start(tc, channel);
+
+  tc->TC_CHANNEL[channel].TC_IER = TC_IER_CPCS;
+  tc->TC_CHANNEL[channel].TC_IDR = ~TC_IER_CPCS;
+  NVIC_EnableIRQ(irq);
+}
+
+void stopTimer(Tc *tc, uint32_t channel, IRQn_Type irq) {
   NVIC_DisableIRQ(irq);
   TC_Stop(tc, channel);
 }
@@ -123,12 +149,6 @@ void stopTimer(Tc* tc, uint32_t channel, IRQn_Type irq) {
 void setup() {
   // start serial port at 115200 bps
   Serial.begin(115200);
-
-  // CAN0 initialization
-  Can0.set_timestamp_capture_point(0);
-  // Can0.disable_time_triggered_mode();
-  Can0.enable_interrupt(CAN_SR_TSTP);
-  Can0.begin(SPEED);
 
   // PIOA power ON
   PMC->PMC_PCER0 |= PMC_PCER0_PID11;
@@ -159,9 +179,17 @@ void setup() {
   SET_LED(false);
   delay(1000);
 
-  Can0.watchFor();
+  // initialize CAN0
+  pmc_enable_periph_clk(ID_CAN0);
+  can_init(CAN0, SystemCoreClock, SPEED);
 
-  // startTimer(TC0, 0, TC0_IRQn, SPEED * 5);
+  // enable only timestamp interrupt and set to fire at SOF
+  can_disable_interrupt(CAN0, CAN_DISABLE_ALL_INTERRUPT_MASK);
+  can_enable_interrupt(CAN0, CAN_SR_TSTP);
+  can_set_timestamp_capture_point(CAN0, 0);
+
+  // enable CAN0 interrupt handler
+  NVIC_EnableIRQ(CAN0_IRQn);
 }
 
 void loop() {
--- makefile
+++ makefile
@@ -1,46 +1,43 @@
-.PHONY: sender receiver hammer monitor due_michican_defender_example due_attacker_example hammer2
+.PHONY: sender receiver hammer monitor due_michican_defender_example due_attacker_example hammer2 hammer2_debug
+
+include config.mk
 
 all: sender hammer2
 
-SENDER_ID   := .port.properties.serialNumber == "75932313638351300251"
-RECEIVER_ID := .port.properties.serialNumber == "85138313234351E09162" or .port.properties.pid == "0x003e"
-HAMMER_ID   := .port.properties.serialNumber == "75932313638351803271"
-# HAMMER_ID   := .port.properties.serialNumber == "85036313130351715042"
-
 sender:
 	@echo "\033[1mcompiling and uploading $@\033[0m"
-	@FLAGS=$$(arduino-cli board list --format=json | jq -r '.[] | select($(SENDER_ID)) | "-b \(.matching_boards | .[0] | .fqbn) -p \(.port.address)"'); \
+	@FLAGS=$$(arduino-cli board list --format=json | jq -r '.detected_ports | .[] | select($(SENDER_ID)) | "-b \(.matching_boards | .[0] | .fqbn) -p \(.port.address)"'); \
 	arduino-cli --no-color compile --warnings=all --upload $$FLAGS $@
 
 receiver:
 	@echo "\033[1mcompiling and uploading $@\033[0m"
-	@FLAGS=$$(arduino-cli board list --format=json | jq -r '.[] | select($(RECEIVER_ID)) | "-b \(.matching_boards | .[0] | .fqbn) -p \(.port.address)"'); \
+	@FLAGS=$$(arduino-cli board list --format=json | jq -r '.detected_ports | .[] | select($(RECEIVER_ID)) | "-b \(.matching_boards | .[0] | .fqbn) -p \(.port.address)"'); \
 	arduino-cli --no-color compile --warnings=all --upload $$FLAGS $@
 
 hammer:
 	@echo "\033[1mcompiling and uploading $@\033[0m"
-	@FLAGS=$$(arduino-cli board list --format=json | jq -r '.[] | select($(HAMMER_ID)) | "-b \(.matching_boards | .[0] | .fqbn) -p \(.port.address)"'); \
+	@FLAGS=$$(arduino-cli board list --format=json | jq -r '.detected_ports | .[] | select($(HAMMER_ID)) | "-b \(.matching_boards | .[0] | .fqbn) -p \(.port.address)"'); \
 	arduino-cli --no-color compile --warnings=all --upload $$FLAGS $@
 
 hammer2:
 	@echo "\033[1mcompiling and uploading $@\033[0m"
-	@FLAGS=$$(arduino-cli board list --format=json | jq -r '.[] | select($(RECEIVER_ID)) | "-b \(.matching_boards | .[0] | .fqbn) -p \(.port.address)"'); \
+	@FLAGS=$$(arduino-cli board list --format=json | jq -r '.detected_ports | .[] | select($(RECEIVER_ID)) | "-b \(.matching_boards | .[0] | .fqbn) -p \(.port.address)"'); \
 	arduino-cli --no-color compile --warnings=all --upload $$FLAGS $@
 
 monitor:
-	@FLAGS=$$(arduino-cli board list --format=json | jq -r '.[] | select(.port.properties.pid == "0x003e") | "-p \(.port.address)"'); \
+	@FLAGS=$$(arduino-cli board list --format=json | jq -r '.detected_ports | .[] | select(.port.properties.pid == "0x003e") | "-p \(.port.address)"'); \
 	arduino-cli monitor $$FLAGS
 
 due_attacker_example:
 	@echo "\033[1mcompiling and uploading $@\033[0m"
-	@FLAGS=$$(arduino-cli board list --format=json | jq -r '.[] | select($(SENDER_ID)) | "-b \(.matching_boards | .[0] | .fqbn) -p \(.port.address)"'); \
+	@FLAGS=$$(arduino-cli board list --format=json | jq -r '.detected_ports | .[] | select($(SENDER_ID)) | "-b \(.matching_boards | .[0] | .fqbn) -p \(.port.address)"'); \
 	arduino-cli --no-color compile --warnings all --upload $$FLAGS $@
 
 due_michican_defender_example:
 	@echo "\033[1mcompiling and uploading $@\033[0m"
-	@FLAGS=$$(arduino-cli board list --format=json | jq -r '.[] | select($(RECEIVER_ID)) | "-b \(.matching_boards | .[0] | .fqbn) -p \(.port.address)"'); \
+	@FLAGS=$$(arduino-cli board list --format=json | jq -r '.detected_ports | .[] | select($(RECEIVER_ID)) | "-b \(.matching_boards | .[0] | .fqbn) -p \(.port.address)"'); \
 	arduino-cli --no-color compile --warnings all --upload $$FLAGS $@
 
 hammer2_debug:
-	@FLAGS=$$(arduino-cli board list --format=json | jq -r '.[] | select($(RECEIVER_ID)) | "-b \(.matching_boards | .[0] | .fqbn) -p \(.port.address)"'); \
+	@FLAGS=$$(arduino-cli board list --format=json | jq -r '.detected_ports | .[] | select($(RECEIVER_ID)) | "-b \(.matching_boards | .[0] | .fqbn) -p \(.port.address)"'); \
 	arduino-cli --no-color compile --warnings=all --export-binaries --optimize-for-debug $$FLAGS hammer2
