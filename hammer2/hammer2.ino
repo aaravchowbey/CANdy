9cb7f1bef56f6305c23f59cd13eedadcc1dfc752
candytwo <candytwo@CANdyTwo>
2024-06-05 15:44:30 -0400

    implemented basic hammering

--- hammer2/hammer2.ino
+++ hammer2/hammer2.ino
@@ -1,30 +1,37 @@
-#include <hmac_sha256.h>
+// TODO: copy frames from CAN1 and send to CAN0 (make sure to not replicate these frames on CAN1 again keeping track with variable)
+// TODO: slight shift of hammering from last few data bytes to CRC
+
+/// #include <hmac_sha256.h>
 
 #define Serial SerialUSB
 
 #define SPEED CAN_BPS_50K
 
-#define HAMMER_BIT_COUNT 4  // total number of bits to hammer for message frame
-#define HAMMER_SIZE 2       // number of bits to hammer per data bit
+#define HAMMER_BIT_COUNT 10  // total number of bits to hammer for message frame
+#define HAMMER_SIZE 1        // number of bits to hammer per data bit
 
 #define SET_LED(state) \
   do { \
-    (state) ? (PIOB->PIO_SODR = PIO_PB27) : (PIOB->PIO_CODR = PIO_PB27); \
     (state) ? (PIOB->PIO_SODR = PIO_PB26) : (PIOB->PIO_CODR = PIO_PB26); \
-  } while (0)
+  } while (0);
 
 #define CANdy_Write(value) \
   do { \
-    (value) ? (PIOB->PIO_SODR = PIO_PB26) : (PIOB->PIO_CODR = PIO_PB26); \
-  } while (0)
-// PIO_PA0A_CANTX0
+    (value) ? (PIOB->PIO_SODR = PIO_PB14A_CANTX1) : (PIOB->PIO_CODR = PIO_PB14A_CANTX1); \
+  } while (0);
+// PIO_PB14A_CANTX1
+
+// (value) ? (PIOB->PIO_SODR = PIO_PB14A_CANTX1) : (PIOB->PIO_CODR = PIO_PB14A_CANTX1);
+
+volatile bool idk = false;
 
 volatile bool hammer_state = false;
 
+volatile bool frame_value = false;
 volatile bool resetValue = false;
 
-volatile uint8_t frame_queue;
-volatile uint8_t bits_after_prev_stuff, frame_bits;
+volatile uint8_t frame_queue = 0;
+volatile uint8_t bits_after_prev_stuff = 0, frame_bits = 0;
 
 // data bits to hammer
 uint8_t hammer_data[32] = { 0b01010101, 0, 0, 0, 0 };
@@ -36,6 +43,8 @@ volatile uint8_t hammer_index = 0;
 // bit in frame has been completely hammered
 volatile bool frameBitHammered = false;
 
+volatile bool checking_for_sof = true;
+
 union {
   volatile uint64_t val;
   uint8_t arr[8];
@@ -43,20 +52,79 @@ union {
 
 volatile uint8_t data_length = 0;
 
+void mailbox_int_handler(uint8_t mb) {
+  if (CAN0->CAN_MB[mb].CAN_MSR & CAN_MSR_MRDY) {       // mailbox signals it is ready
+    switch (((CAN0->CAN_MB[mb].CAN_MMR >> 24) & 7)) {  // what sort of mailbox is it?
+      case 1:                                          // receive
+      case 2:                                          // receive w/ overwrite
+      case 4:                                          // consumer - technically still a receive buffer
+        can_mailbox_send_transfer_cmd(CAN0, mb);
+        break;
+      case 3:  //transmit
+        can_disable_interrupt(CAN0, 0x01 << mb);
+        break;
+      case 5:  //producer - technically still a transmit buffer
+        break;
+    }
+  }
+}
+
 void CAN0_Handler() {
+  uint32_t ul_status = CAN0->CAN_SR;  //get status of interrupts
+
+  if (ul_status & CAN_SR_MB0) {  //mailbox 0 event
+    mailbox_int_handler(0);
+  }
+  if (ul_status & CAN_SR_MB1) {  //mailbox 1 event
+    mailbox_int_handler(1);
+  }
+  if (ul_status & CAN_SR_MB2) {  //mailbox 2 event
+    mailbox_int_handler(2);
+  }
+  if (ul_status & CAN_SR_MB3) {  //mailbox 3 event
+    mailbox_int_handler(3);
+  }
+  if (ul_status & CAN_SR_MB4) {  //mailbox 4 event
+    mailbox_int_handler(4);
+  }
+  if (ul_status & CAN_SR_MB5) {  //mailbox 5 event
+    mailbox_int_handler(5);
+  }
+  if (ul_status & CAN_SR_MB6) {  //mailbox 6 event
+    mailbox_int_handler(6);
+  }
+  if (ul_status & CAN_SR_MB7) {  //mailbox 7 event
+    mailbox_int_handler(7);
+  }
+
   // runs on second frame bit at ~70%
-  if (CAN0->CAN_SR & CAN_SR_TSTP) {
-    // fires on third frame bit at ~70%
-    startTimer(TC0, 0, TC0_IRQn, SPEED * 1000);
+  if (ul_status & CAN_SR_TSTP) {
+    if (checking_for_sof) {
+      checking_for_sof = false;
+      can_set_timestamp_capture_point(CAN0, 1);
 
-    // set frame_queue
-    bool value = PIOA->PIO_PDSR & PIO_PA1A_CANRX0;
-    frame_queue = 0b11111111111111111111111111111100 | value;
+      // fires on third frame bit at ~70%
+      startTimer(TC0, 0, TC0_IRQn, SPEED * 1000);
 
-    // reset values
-    bits_after_prev_stuff = 2;
-    frame_bits = 2;
-    hammer_index = 0;
+      PIOB->PIO_PER = PIO_PB14A_CANTX1;
+
+      // set frame_queue
+      frame_value = 0;
+      frame_queue = 0b11111110;
+      CANdy_Write(0);
+
+      // reset values
+      bits_after_prev_stuff = 1;
+      frame_bits = 1;
+      hammer_index = 0;
+    } else {
+      // SET_LED(idk);
+      // idk = !idk;
+      checking_for_sof = true;
+      can_set_timestamp_capture_point(CAN0, 0);
+      stopTimer(TC0, 0, TC0_IRQn);
+      PIOB->PIO_PDR = PIO_PB14A_CANTX1;
+    }
   }
 }
 
@@ -64,38 +132,44 @@ void CAN0_Handler() {
 void TC0_Handler() {
   TC_GetStatus(TC0, 0);
 
-  bool value = PIOA->PIO_PDSR & PIO_PA1A_CANRX0;
+  CANdy_Write(frame_value);
+  frame_value = PIOA->PIO_PDSR & PIO_PA1A_CANRX0;
 
-  if (bits_after_prev_stuff >= 5 && ((value == 0 && (frame_queue & 0b11111) == 0b11111) || (value == 1 && (frame_queue & 0b11111) == 0))) {
+  if (bits_after_prev_stuff >= 5 && ((frame_value == 0 && (frame_queue & 0b11111) == 0b11111) || (frame_value == 1 && (frame_queue & 0b11111) == 0))) {
     // if stuff bit, reset frame_samples
     bits_after_prev_stuff = 0;
   } else {
     // if not stuff bit, add to frame_queue
-    frame_queue = ((frame_queue & 0b1111111) << 1) | value;
+    frame_queue = ((frame_queue & 0b1111111) << 1) | frame_value;
 
     // increment counters
     bits_after_prev_stuff++;
     frame_bits++;
 
-    if (frame_bits == 15 && (value & 0b111) != 0) {
-      stopTimer(TC0, 0, TC0_IRQn);
+    if (frame_bits == 15 && (frame_value & 0b111) != 0) {
+      SET_LED(idk);
+      idk = !idk;
+      // TODO: figure out why stopTimer fails to work here
+      // stopTimer(TC0, 0, TC0_IRQn);
     } else if (frame_bits == 19) {
       if ((frame_queue & 0b1111) != 0) {
         data_length = frame_queue & 0b1111;
         frame_data.val = 0;
       } else {
         data_length = 0;
-        stopTimer(TC0, 0, TC0_IRQn);
+        // TODO: figure out why stopTimer fails to work here
+        // stopTimer(TC0, 0, TC0_IRQn);
       }
     } else if (data_length != 0 && frame_bits > 19 && frame_bits <= 19 + data_length * 8) {
-      frame_data.val = (frame_data.val << 1) | value;
+      frame_data.val = (frame_data.val << 1) | frame_value;
+
+      // SET_LED(idk);
+      // idk = !idk;
 
       if (frame_bits == 19 + data_length * 8) {
-        hammer_data[0] = 4;
+        // hammer_data[0] = 4;
         // hmac_sha256(key, sizeof(key) - 1, frame_data.arr, data_length, hammer_data, 32);
-        startTimer(TC1, 0, TC3_IRQn, (uint32_t)(SPEED * 1000 * 3.333));
-
-        stopTimer(TC0, 0, TC0_IRQn);
+        startTimer(TC1, 0, TC3_IRQn, (uint32_t)(SPEED * 1000 * 3.3333));
       }
     }
   }
@@ -108,10 +182,6 @@ void TC3_Handler() {
   if (hammer_index < HAMMER_BIT_COUNT) {
     // starts TC4 at next data bit (~0% mark)
     startTimer(TC1, 1, TC4_IRQn, SPEED * 1000);
-
-    // turn on multiplexing
-    // PIOA->PIO_PER = PIO_PA0A_CANTX0;
-    // PIOA->PIO_OER = PIO_PA0A_CANTX0;
 
     resetValue = PIOA->PIO_PDSR & PIO_PA1A_CANRX0;
     frameBitHammered = false;
@@ -132,10 +202,6 @@ void TC4_Handler() {
     // if bits left to hammer, setup hammering for one data bit
     resetValue = PIOA->PIO_PDSR & PIO_PA1A_CANRX0;
     frameBitHammered = false;
-
-    // turn on multiplexing
-    // PIOA->PIO_PER = PIO_PA0A_CANTX0;
-    // PIOA->PIO_OER = PIO_PA0A_CANTX0;
 
     // start TC7 to fire 5 times in a bit (~20% mark)
     startTimer(TC2, 0, TC6_IRQn, SPEED * 1000 * 5);
@@ -162,10 +228,6 @@ void TC6_Handler() {
 
     // stop TC6
     stopTimer(TC2, 0, TC6_IRQn);
-
-    // turn off multiplexing
-    // PIOA->PIO_PDR = PIO_PA0A_CANTX0;
-    // PIOA->PIO_ODR = PIO_PA0A_CANTX0;
   }
 }
 
@@ -219,18 +281,54 @@ void setup() {
   // enable CAN0 interrupt handler
   NVIC_EnableIRQ(CAN0_IRQn);
 
+  // init RX boxen
+  int c;
+  for (c = 0; c < 8 - 1; c++) {
+    // set mode to CAN_MB_RX_MODE
+    CAN0->CAN_MB[c].CAN_MMR = (CAN0->CAN_MB[c].CAN_MMR & ~CAN_MMR_MOT_Msk) | (CAN_MB_RX_MODE << CAN_MMR_MOT_Pos);
+
+    // set id to 0x0
+    CAN0->CAN_MB[c].CAN_MID = CAN_MID_MIDvA(0x0);
+
+    // set accept mask to 0x7ff
+    CAN0->CAN_MB[c].CAN_MAM = CAN_MAM_MIDvA(0x7ff);
+    CAN0->CAN_MB[c].CAN_MID &= ~CAN_MAM_MIDE;
+  }
+
+  //Initialize TX boxen
+  for (c = 8 - 1; c < 8; c++) {
+    // set mode to CAN_MB_TX_MODE
+    CAN0->CAN_MB[c].CAN_MMR = (CAN0->CAN_MB[c].CAN_MMR & ~CAN_MMR_MOT_Msk) | (CAN_MB_TX_MODE << CAN_MMR_MOT_Pos);
+
+    // set priority to 10
+    CAN0->CAN_MB[c].CAN_MMR = (CAN0->CAN_MB[c].CAN_MMR & ~CAN_MMR_PRIOR_Msk) | (10 << CAN_MMR_PRIOR_Pos);
+
+    // set accept mask to 0x7ff
+    CAN0->CAN_MB[c].CAN_MAM = CAN_MAM_MIDvA(0x7ff);
+    CAN0->CAN_MB[c].CAN_MID &= ~CAN_MAM_MIDE;
+  }
+
+  CAN0->CAN_MB[0].CAN_MAM = CAN_MAM_MIDvA(0);
+  CAN0->CAN_MB[0].CAN_MID &= ~CAN_MAM_MIDE;
+  CAN0->CAN_MB[0].CAN_MID = CAN_MID_MIDvA(0);
+  can_enable_interrupt(CAN0, CAN_IER_MB0);
+
   // PIOA power ON
   PMC->PMC_PCER0 |= PMC_PCER0_PID11;
 
-  // multiplex CAN_RX to GPIO (Peripheral Enable Register)
-  PIOA->PIO_PER = PIO_PA1A_CANRX0;
-
-  // set CAN_RX as input (Output Disable Register)
-  PIOA->PIO_ODR = PIO_PA1A_CANRX0;
-
   // disable pull-up on both pins (Pull-Up Disable Register)
-  PIOA->PIO_PUDR = PIO_PA1A_CANRX0;
   PIOA->PIO_PUDR = PIO_PA0A_CANTX0;
+  PIOA->PIO_PDR = PIO_PA0A_CANTX0;
+  PIOA->PIO_OER = PIO_PA0A_CANTX0;
+
+  // turn on multiplexing
+  PIOB->PIO_PUDR = PIO_PB14A_CANTX1;
+  PIOB->PIO_PDR = PIO_PB14A_CANTX1;
+  PIOB->PIO_OER = PIO_PB14A_CANTX1;
+
+  //Initialize CAN1
+  pmc_enable_periph_clk(ID_CAN1);
+  can_init(CAN1, SystemCoreClock, SPEED);
 }
 
 void loop() {
